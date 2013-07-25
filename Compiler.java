51eb16d7367a725444d7106a8558564f55885acd
g3org3 <7jagjag@gmail.com>
2013-07-25 00:38:51 -0600

    Ayuda terminado

--- Compiler.java
+++ Compiler.java
@@ -24,16 +24,38 @@ public class Compiler {
 		error(o, d);
 	}
 	public void error(int o, int d){
-		System.out.println("----------------------------------------------------");
+		//System.out.println("-------------------------------------------------------------------------------------");
 		//	DEBUG
 		System.out.println("*Error on line "+d+".");
-		System.out.println("\t\t\t::Ayuda::");
-		System.out.println("\t-o\t<outname>\t\t<outname> es el archivo");
-		System.out.println("\t-target\t<stage>\t\t\t<stage> es el archivo");
-		System.out.println("\t-opt\t<optimization>\t\t<optimization> es el archivo");
-		System.out.println("\t-debug\t<stage>\t\t\t<stage> es el archivo");
-		System.out.println("\t-h\t\t\t la opcion para desplegar el menu de ayuda.");
-		System.out.println("----------------------------------------------------");
+		System.out.println("\n\t\t\t\t::Ayuda::");
+		System.out.println("Para poder ejecuar el compilador correctamente necesita ingresar en la linea de");
+		System.out.println("comandos 'java Compiler <opcion> <filename>' donde <opcion> puede ser una lista");
+		System.out.println("de opciones que se le otorga a usted el usuario para el compilador, las opciones");
+		System.out.println("se le especifican con detalle adelante. Y <filename> sera el archivo el cual");
+		System.out.println("sera compilado y tiene que ser ingresado correctamente como '(filename).txt'.");
+		System.out.println(" ");
+		System.out.println("-------------------------------------------------------------------------------------");
+		System.out.println("\t\t\t\t::Opciones::");
+		System.out.println("Estas son las opciones que seran aceptadas por el compilador:");
+		System.out.println(" ");
+	    System.out.println("-o\t<outname>\tEsta opcion es la cual <outname> es el archivo que sera");
+		System.out.println("\t\t\tel destino donde el compilador dejara lo generado.");
+		System.out.println(" ");
+		System.out.println("-target\t<stage>\t\tEsta opcion lo que realiza es que lleva a un punto en ");
+		System.out.println("\t\t\tespecifico el compilador de su eleccion donde <stage> ");
+		System.out.println("\t\t\tpodria ser (scan, parse, ast, irt, semantic o codegen).");
+		System.out.println(" ");
+		System.out.println("-opt\t<optimization>\tEsta opcion es la cual optimiza el compilador y ");
+		System.out.println("\t\t\t<optimization> puede ser constant o algebraic.");
+		System.out.println(" ");
+		System.out.println("-debug\t<stage>\t\tEsta opcion es la cual debuguea su programa o archivo ");
+		System.out.println("\t\t\tingresado y <stage> hasta donde usted desea que se");
+		System.out.println("\t\t\tdebuguee y puede ser ingresado de esta manera ");
+		System.out.println("\t\t\t'scan:parse:ast:irt:semantic:codegen:'.");
+		System.out.println(" ");
+		System.out.println("-h\t\t\tEsta opcion es la cual usted utiliza para que se le ");
+		System.out.println("\t\t\totorgue la ayuda.");
+		System.out.println("-------------------------------------------------------------------------------------");
 		System.exit(1);
 	}
 	public void noArguments(){
