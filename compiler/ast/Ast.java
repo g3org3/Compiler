b28f24213d60b7f8bad18768a6eeb9a0f1cafa48
g3org3 <7jagjag@gmail.com>
2013-08-01 13:06:38 -0600

    update

--- compiler/ast/Ast.java
+++ compiler/ast/Ast.java
@@ -1,13 +1,13 @@
 package compiler.ast;
 
-import compiler.parser.CC4Paser;
+import compiler.parser.CC4Parser;
 
 public class Ast {
 	
-	public CC4Paser parser;
+	public CC4Parser par;
 
 	// contructor
-	public Ast(CC4Paser ps){
-		this.parser = ps;
+	public Ast(CC4Parser ps){
+		this.par = ps;
 	}
 }
